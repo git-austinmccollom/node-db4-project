b276e8af15564a4b2af257db3006638d58d97317
git-austinmccollom <austinmccollom@gmail.com>
2020-10-08 16:05:20 -0500

    cleanup seed and schema

--- migrations/20201008152034_init.js
+++ migrations/20201008152034_init.js
@@ -1,8 +1,39 @@
+const { table } = require("../database/connection");
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("recipe", (tbl) => {
+      tbl.increments();
+      tbl.string("recipe_name").notNullable();
+    })
+    .createTable("ingredient", (tbl) => {
+      tbl.increments();
+      tbl.string("ingredient_name").notNullable();
+    })
+    .createTable("recipe_ingredient", (tbl) => {
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("recipe")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      tbl
+        .integer("ingredient_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("ingredient")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      tbl.primary(["recipe_id", "ingredient_id"]);
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("recipe_ingredient")
+    .dropTableIfExists("ingredient")
+    .dropTableIfExists("recipe");
 };
