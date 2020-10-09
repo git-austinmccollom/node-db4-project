509f6b90e96492acf8351cb4848e41bdb59aed11
git-austinmccollom <austinmccollom@gmail.com>
2020-10-08 20:08:43 -0500

    shopping list feature

--- dbAccessFunctions.js
+++ dbAccessFunctions.js
@@ -1,7 +1,23 @@
 const db = require('./database/connection.js');
 
 module.exports = {
-    getRecipes(),
-    // getShoppingList(),
-    // getInstructions()
+    getRecipes,
+    getShoppingList
+    // getInstructions
 }
+
+function getRecipes() {
+    return db('recipe')
+}
+
+function getShoppingList(recipe_id) {
+    return db.select('ingredient.ingredient_name', 'recipe_ingredient.quantity' )
+    .from('ingredient')
+    .join('recipe_ingredient', 'ingredient.id', '=', 'recipe_ingredient.ingredient_id')
+    .where('recipe_id', recipe_id)
+}
+
+// SELECT ingredient.ingredient_name, recipe_ingredient.quantity
+// FROM ingredient
+// JOIN recipe_ingredient ON ingredient.id = recipe_ingredient.ingredient_id
+// WHERE recipe_id = 1
--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
+const dbFun = require("./dbAccessFunctions.js");
 
 const server = express();
 server.use(helmet());
 server.use(express.json());
 
-server.get("/", (req, res) => {
-    res.status(200).json({hello: "Hello World" });
+server.get("/api/recipes/", (req, res) => {
+  dbFun
+    .getRecipes()
+    .then((dbRes) => {
+      res.status(200).json(dbRes);
+    })
+    .catch((dbErr) => {
+      res.status(500).json(dbErr);
+    });
+});
+
+server.get("/api/recipes/:id/shoppingList", (req, res) => {
+    dbFun.getShoppingList(req.params.id)
+    .then((dbRes) => {
+        res.status(200).json(dbRes);
+      })
+      .catch((dbErr) => {
+        res.status(500).json(dbErr);
+      });
 })
 
-const PORT = process.env.PORT || 5000
+server.get("/", (req, res) => {
+  res.status(200).json({ hello: "Hello World" });
+});
+
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`API running on port ${PORT}`));
